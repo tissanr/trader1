6c86f24734e5c7194ba77cea1fd31b5c47c9f263
Stephan Reiter <stephan@Mac.fritz.box>
2026-02-24 17:40:34 +0100

    dependencies and account balance

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [clj-http "3.9.1"]
-                 [cheshire "5.8.0"];;JSON
-                 [clj-time "0.15.0"]
-                 [digest "1.4.8"]
-                 [org.clojure/data.codec "0.1.1"]]
+  :dependencies [[org.clojure/clojure "1.11.4"]
+                 [clj-http "3.12.3"]
+                 [cheshire "5.13.0"];;JSON
+                 [clj-time "0.15.2"]
+                 [digest "1.4.10"]
+                 [org.clojure/data.codec "0.1.0"]]
   :main ^:skip-aot trader1.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
